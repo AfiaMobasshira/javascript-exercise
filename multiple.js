3d4dffd6e6f2d838ff549e9ea593d8bc7fd7a28c
AfiaMobasshira <arthyafiya93@gmail.com>
2026-02-21 21:30:01 +0600

    add discount

--- multiple.js
+++ multiple.js
@@ -1,10 +1,16 @@
-const price = 6000;
+const price = 9000;
 
-if( price >= 5000 ){
+if( price >= 4000 ){
     // 10% discount
     const discount = price * 10 / 100;
-    const payMent = price - discount;
-    console.log(payMent);
+    const payment = price - discount;
+    console.log(payment);
+}
+else if( price >= 10000 ){
+    // 30% discount
+    const discount = price * 30 / 100;
+    const payment = price - discount;
+    console.log(payment);
 }
 else{
     console.log(price);
